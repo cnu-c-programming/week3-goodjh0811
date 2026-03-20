3cfda39efaade7baf818f6a606cbad10e8bdbf19
박지현202500924 <goodjh0811@gmail.com>
2026-03-20 09:40:40 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int power(int base, int exp){
+
+}
+
+int main(){
+  printf("%d\n", power(2, 3));
+  printf("%d\n", power(5, 0));
+  printf("%d\n", power(3, 4));
+
+  return 0;
+
+}
+  
