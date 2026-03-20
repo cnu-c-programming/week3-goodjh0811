d0c02e19ecaad9bfedaa810c08476c4a1086724c
박지현202500924 <goodjh0811@gmail.com>
2026-03-20 09:51:41 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
-int add(int a, int b){
-  return a + b;
+int add(int x, int y){
+  return x + y;
 }
-int sub(int a, int b){
-  return a - b;
+int sub(int x, int y){
+  return x - y;
 }
-int mul(int a, int b){
-  return a * b;
+int mul(int x, int y){
+  return x * y;
 }
-int div(int a, int b){
-  return a\b;
+int div(int x, int y){
+  return x / y;
 }
 int main (void){
   int a = 10, b = 5;
+
+  printf("%d + %d = %d\n", a, b, add(a, b));
+  printf("%d - %d = %d\n", a, b, sub(a, b));
+  printf("%d * %d = %d\n", a, b, mul(a, b));
+  printf("%d / %d = %d\n", a, b, div(a, b));
 
 return 0;
 }
