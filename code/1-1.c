145ba268188570f12cbffae5772787ffee4f53ed
박지현202500924 <goodjh0811@gmail.com>
2026-03-20 09:32:34 +0900

    Update 1-1.c

---
+++ code/1-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int add(int a, int b){
+  return a + b;
+}
+int sub(int a, int b){
+  return a - b;
+}
+int mul(int a, int b){
+  return a * b;
+}
+int div(int a, int b){
+  return a\b;
+}
+int main (void){
+  int a = 10, b = 5;
+
+return 0;
+}
